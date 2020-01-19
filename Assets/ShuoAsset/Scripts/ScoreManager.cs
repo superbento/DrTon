5d56b0202efdcd260e7b3aa9ddaa9423c3595685
superbento <37655893+superbento@users.noreply.github.com>
2020-01-20 00:11:32 +0100

    add offset to soundtrack

---
+++ Assets/ShuoAsset/Scripts/ScoreManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int score;
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
