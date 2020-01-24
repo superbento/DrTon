6d442eb3d1cfa057e727dffbe85817857d0ec9cc
MiaoMiaoMeng <billknightms@gmail.com>
2020-01-24 16:00:29 +0100

    finish

---
+++ Assets/4K_Space_Skyboxes/Demo/Scripts/Skybox_rotator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Skybox_rotator : MonoBehaviour
+{
+    public Text skyboxNameText;
+    public List<Material> skyboxes;
+    private int currentSkybox = 0;
+
+    void Start()
+    {
+        SetSkybox();
+    }
+
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // increment the skybox
+            currentSkybox++;
+            if (currentSkybox >= skyboxes.Count)
+            {
+                // loop round to the first skybox if we have reached the last skybox
+                currentSkybox = 0;
+            }
+
+            SetSkybox();
+        }
+	}
+
+    void SetSkybox()
+    {
+        // set the skybox
+        RenderSettings.skybox = skyboxes[currentSkybox];
+        skyboxNameText.text = skyboxes[currentSkybox].name;
+    }
+}
